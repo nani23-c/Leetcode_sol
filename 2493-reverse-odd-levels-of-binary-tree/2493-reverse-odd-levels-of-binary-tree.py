c317ab772aae3f2560d0e8300066bc100b7d4d56
TEJDEEP RAGALA <95916886+nani23-c@users.noreply.github.com>
2024-12-21 22:36:27 +0530

    Time: 27 ms (41.88%), Space: 22.8 MB (6.41%) - LeetHub

--- 2493-reverse-odd-levels-of-binary-tree/2493-reverse-odd-levels-of-binary-tree.py
+++ 2493-reverse-odd-levels-of-binary-tree/2493-reverse-odd-levels-of-binary-tree.py
@@ -6,30 +6,13 @@
 #         self.right = right
 class Solution:
     def reverseOddLevels(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        ans = []
-        ans_level = [0]
-        def LevelOrder(Node,Level):
-            if Level==ans_level[0]:
-                ans_level[0]+=1
-                ans.append([])
-            ans[Level].append(Node.val)
-            if Node.left:  
-                Level+=1
-                LevelOrder(Node.left,Level)
-                LevelOrder(Node.right,Level)
-        LevelOrder(root,0)
-        for i in range(len(ans)):
-            if i%2!=0:
-                ans[i].reverse()
-        print(ans)
-        def Equal(Node,Level):
-            Node.val=ans[Level].pop(0)
-            if Node.left:
-                Level+=1
-                Equal(Node.left,Level)
-                Equal(Node.right,Level)
-        Equal(root,0)
+        def Traverse(leftnode,rightnode,Level):
+            if leftnode:
+                if Level%2==1:
+                    leftnode.val,rightnode.val=rightnode.val,leftnode.val
+                Traverse(leftnode.left,rightnode.right,Level+1)
+                Traverse(leftnode.right,rightnode.left,Level+1)
+        Traverse(root.left,root.right,1)
         return root
-
 
             
